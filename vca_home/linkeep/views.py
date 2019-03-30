93cc682b13cbbbcd9b2759b14d8ccb1b5ec0cd36
Pedro Vicari <peehvicari@gmail.com>
2019-03-30 18:07:12 -0300

    Alteracoes na view card

--- vca_home/linkeep/views.py
+++ vca_home/linkeep/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Card
 
 # Create your views here.
 
 def card_list(request):
-    return render(request, 'linkeep/card_list.html', {})
-    
+    cards = Card.objects.all().order_by('created_date')
+    return render(request, 'linkeep/card_list.html', {'cards': cards})
